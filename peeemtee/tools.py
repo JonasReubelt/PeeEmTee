174e180d7877b963ecea1eabf2680b14a7d72392
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2018-12-20 16:53:08 +0100

    adds persistense histogram functionality

--- peeemtee/tools.py
+++ peeemtee/tools.py
@@ -59,3 +59,41 @@ def calculate_histogram_data(data, bins=10, range=None):
     x = x[:-1]
     x = x + (x[1] - x[0]) / 2
     return x, y
+
+
+def calculate_persist_data(waveforms, bins=(10, 10), range=None):
+    """
+    Calculates 2D histogram data like persistence mode on oscilloscope
+
+    Parameters
+    ----------
+    waveforms: np.array
+        2D numpy array with one waveform in each row
+        [[waveform1],
+        [waveform2],
+        ...]
+    bins: tuple(int)
+        number of bins in both directions
+    range: tuple(tuple(int))
+        lower and upper range of the x-bins and y-bins
+
+    Returns
+    -------
+    x: np.array
+        x-bin centres of the histogram
+    y: np.array
+        y-bin centres of the histogram
+    z: np.array
+        z values of the histogram
+
+    """
+    times = np.tile(np.arange(waveform.shape[1]), (waveforms.shape[0], 1))
+    z, xs, ys = np.histogram2d(times.flatten(),
+                                   waveforms.flatten(),
+                                   bins=bins,
+                                   range=range)
+    xs = (xs + (xs[1] - xs[0])/2)[:-1]
+    ys = (ys + (ys[1] - ys[0])/2)[:-1]
+    x = np.array([[x] * bins[0] for x in xs])
+    y = np.array(list(ys) * bins[1])
+    return x, y, z
