7bbf414a63e4fccef8570cdcf4574fba3b8082a2
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2019-04-04 12:04:11 +0200

    adds qe of thor phd to constants

--- peeemtee/constants.py
+++ peeemtee/constants.py
@@ -4,3 +4,6 @@ import numpy as np
 DATA_DIR = join(dirname(__file__), "data")
 
 hama_phd_qe = np.loadtxt(join(DATA_DIR, "phdqe.txt"))
+
+thor_data = np.loadtxt(join(DATA_DIR, "phdqe_thorlabs.txt"), unpack=True)
+thor_phd_qe = np.array((thor_data[0], thor_data[2])).T
