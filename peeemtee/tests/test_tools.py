adds test to transit time calculation and fixes bug

peeemtee/tests/test_tools.py
@@ -6,10 +6,27 @@ from peeemtee.tools import (
     peak_finder,
     gaussian,
     gaussian_with_offset,
+    calculate_transit_times,
 )
 
 
 class TestTools(TestCase):
+    def test_gaussian(self):
+        assert gaussian(0, 0, 1, 1) == 0.3989422804014327
+        assert gaussian(0.345, 1.234, 0.5432, 108) == 20.78525811770294
+        assert gaussian(1.098, -1.342, 12.34, 1029387.234) == 32635.01097991775
+
+    def test_gaussian_with_offset(self):
+        assert gaussian_with_offset(0, 0, 1, 1, 1) == 1.3989422804014327
+        assert (
+            gaussian_with_offset(1.2234, -2.34, 2.345, 123.23, -12.4)
+            == -5.792028722690032
+        )
+        assert (
+            gaussian_with_offset(-0.9857, 12.34, 24.345, 123.23, 34.4)
+            == 36.13842765114078
+        )
+
     def test_calculate_charges(self):
         data = np.array(
             [
@@ -20,6 +37,18 @@ class TestTools(TestCase):
         )
         self.assertListEqual(
             list(calculate_charges(data, 0, 2, 2, 4)), [99, 396, 56]
+        )
+
+    def test_calculate_transit_times(self):
+        data = np.array(
+            [
+                [0, -1, 1, -53, 0, 1],
+                [-5, 3, -14, -253, 143, 3],
+                [100, 102, 85, -9, -1, -15],
+            ]
+        )
+        self.assertListEqual(
+            list(calculate_transit_times(data, 0, 2, -10)), [3, 2, 2]
         )
 
     def test_bin_data(self):
@@ -48,19 +77,3 @@ class TestTools(TestCase):
         peak_positions = peak_finder(test_waveforms, -1)
         result = [[4.0, 11.5], [2.5], [15.0], [0.0]]
         self.assertListEqual(peak_positions, result)
-
-    def test_gaussian(self):
-        assert gaussian(0, 0, 1, 1) == 0.3989422804014327
-        assert gaussian(0.345, 1.234, 0.5432, 108) == 20.78525811770294
-        assert gaussian(1.098, -1.342, 12.34, 1029387.234) == 32635.01097991775
-
-    def test_gaussian_with_offset(self):
-        assert gaussian_with_offset(0, 0, 1, 1, 1) == 1.3989422804014327
-        assert (
-            gaussian_with_offset(1.2234, -2.34, 2.345, 123.23, -12.4)
-            == -5.792028722690032
-        )
-        assert (
-            gaussian_with_offset(-0.9857, 12.34, 24.345, 123.23, 34.4)
-            == 36.13842765114078
-        )

peeemtee/tools.py
@@ -84,7 +84,9 @@ def calculate_transit_times(signals, baseline_min, baseline_max, threshold):
         1D array with transit times matching axis 0 of the signals array
 
     """
-    zeroed_signals = signals - np.mean(signals[:, baseline_min:baseline_max])
+    zeroed_signals = (
+        signals.T - np.mean(signals[:, baseline_min:baseline_max], axis=1)
+    ).T
     transit_times = np.argmax(zeroed_signals < threshold, axis=1)
     return transit_times[transit_times != 0]
 