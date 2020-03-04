1e32dfd3833bc855d65565d3f0cb276d192b103e
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2020-03-04 13:44:52 +0100

    adapts test

--- peeemtee/tests/test_core.py
+++ peeemtee/tests/test_core.py
@@ -7,8 +7,8 @@ class TestTools(TestCase):
     def test_WavesetReader(self):
         reader = WavesetReader("peeemtee/tests/samples/waveform_data_dummy.h5")
         wavesets = reader.wavesets
-        hvs = [900.0, 950.0, 1000.0, 1050.0, 1100.0]
-        assert sorted(wavesets) == hvs
+        hvs = ["900", "950", "1000", "1050", "1100"]
+        assert sorted(wavesets) == sorted(hvs)
         for i, hv in enumerate(hvs):
             waveset = reader[int(hv)]
             a = np.arange(1, 10, dtype=np.int8).reshape(3, 3) + i * 10
