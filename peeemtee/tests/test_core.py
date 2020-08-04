fff15832e19821208b4777b66959e180594c01bc
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2020-08-04 15:17:28 +0200

    ... and adds a test for it

--- peeemtee/tests/test_core.py
+++ peeemtee/tests/test_core.py
@@ -9,6 +9,7 @@ class TestTools(TestCase):
         wavesets = reader.wavesets
         hvs = ["900", "950", "1000", "1050", "1100"]
         assert sorted(wavesets) == sorted(hvs)
+        b = np.array([[0, 1, 2], [0, 1, 2], [0, 1, 2]])
         for i, hv in enumerate(hvs):
             waveset = reader[int(hv)]
             a = np.arange(1, 10, dtype=np.int8).reshape(3, 3) + i * 10
@@ -17,3 +18,6 @@ class TestTools(TestCase):
             assert v_gain == i * 0.1
             np.testing.assert_array_equal(waveset.waveforms, a * v_gain)
             assert waveset.h_int == i * 10
+            np.testing.assert_array_almost_equal(
+                waveset.zeroed_waveforms(0, 1), b * v_gain
+            )
